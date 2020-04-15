7b0443239616f6cc337c937df514525fc94c99ab
SinanDYG <sinan.babacan@dygdigital.com>
2020-04-15 15:12:10 +0300

    created.

---
+++ ImageResizerCore.AspNet/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace ImageResizerCore.AspNet
+{
+    public static class Extensions
+    {
+        public static IApplicationBuilder ImageResizerCore(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ImageResizerCoreMiddleware>();
+        }
+    }
+}
---
+++ ImageResizerCore.AspNet/ImageResizerCoreMiddleware.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ImageResizerCore.AspNet
+{
+    public class ImageResizerCoreMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IHostingEnvironment _environment;
+
+        public ImageResizerCoreMiddleware(RequestDelegate next, IHostingEnvironment environment)
+        {
+            _next = next;
+            _environment = environment;
+        }
+    }
+}
